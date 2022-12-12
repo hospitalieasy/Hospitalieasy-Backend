4a02fd5850a6daa20b0a5f1180b9047c978f6e35
EgeKaratas <segekaratas@gmail.com>
2022-12-12 14:17:37 +0300

    Patient attributes updated

--- HospitalEasyDemo/Models/Patient.cs
+++ HospitalEasyDemo/Models/Patient.cs
@@ -4,7 +4,15 @@
     {
         public int PatientId { get; set; }
 
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string BirthDate { get; set; }
         public string Email { get; set; }
         public int Password { get; set; }
+
+        public string Telephone { get; set; }
+
     }
 }
