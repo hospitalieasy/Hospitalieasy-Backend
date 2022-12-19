4274deeb7ed2f5344bfa1394f9d883fc1626205e
Bestesakar <beste.sakar@reateknoloji.com>
2022-12-19 21:02:13 +0300

    Added encryption and decryption methods.

--- HospitalEasyDemo/Controllers/PatientController.cs
+++ HospitalEasyDemo/Controllers/PatientController.cs
@@ -157,6 +157,34 @@ namespace HospitalEasyDemo.Controllers
         }
 
 
+        /* public static string EncryptPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+            else
+            {
+                byte[] storePassword = ASCIIEncoding.ASCII.GetBytes(password);
+                string encryptedPassword = Convert.ToBase64String(storePassword);
+                return encryptedPassword;
+            }
+        }
 
+        public static string DecryptPassword(string password)
+        {
+
+            if (string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+            else
+            {
+                byte[] encryptedPassword = Convert.FromBase64String(password);
+                string decryptedPassword = ASCIIEncoding.ASCII.GetString(encryptedPassword);
+                return decryptedPassword;
+            }
+        }
+        */
     }
 }
